7e71eb062d1914929f7099c5364ac5975cf6932e
romtom35 <romtom35@gmail.com>
2019-02-01 11:08:10 +0100

    changement des condition du fichier temperature.py

--- temperature.py
+++ temperature.py
@@ -4,11 +4,11 @@ capteur = TemperatureSensor('28-021313bf0eaa')
 
 
 type_temp = input('Quelle unite de température voulez-vous chosir (F ou C) : ')
-if 'C':
+if type_temp == 'C':
     while True:
         print(capteur.read_temp())
         time.sleep(1)
-elif 'F':
+elif type_temp == 'F':
     while True:
         print(capteur.read_temp_far())
         time.sleep(1)
